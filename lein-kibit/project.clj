a6ccfef2f2462a2067d81676d95b3a8935608c0f
ertucetin <ertu.ctn@gmail.com>
2020-04-20 23:47:45 +0300

    update lein plugin pisano kibit

--- lein-kibit/project.clj
+++ lein-kibit/project.clj
@@ -1,11 +1,12 @@
-(defproject lein-kibit (clojure.string/trim-newline (slurp "../kibit-common/resources/jonase/kibit/VERSION"))
+(defproject pisano/lein-kibit "0.1.0"
   :description "kibit lein plugin"
   :url "https://github.com/jonase/kibit"
-  :resource-paths ["../kibit-common/resources"]
+  ;:resource-paths ["../kibit-common/resources"]
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                 [jonase/kibit ~(clojure.string/trim-newline (slurp "../kibit-common/resources/jonase/kibit/VERSION"))]]
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
+                 [pisano/kibit "0.1.0"]]
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["releases" {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :eval-in-leiningen true)
